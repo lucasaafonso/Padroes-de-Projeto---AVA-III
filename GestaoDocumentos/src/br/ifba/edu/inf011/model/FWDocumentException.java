50b9cbdf9bffe605a2a7b2742fd1e85a6ab2b7ec
lucasaafonso <lucasaafonso103@gmail.com>
2026-01-23 20:33:06 -0300

    Código original de Fred

---
+++ GestaoDocumentos/src/br/ifba/edu/inf011/model/FWDocumentException.java
@@ -0,0 +1,7 @@
+package br.ifba.edu.inf011.model;
+
+public class FWDocumentException extends Exception {
+	public FWDocumentException(String s) {
+		super(s);
+	}
+}
---
+++ GestaoDocumentos/src/br/ifba/edu/inf011/prototype/Prototipavel.java
@@ -0,0 +1,7 @@
+package br.ifba.edu.inf011.prototype;
+
+import br.ifba.edu.inf011.model.FWDocumentException;
+
+public interface Prototipavel {
+	public Prototipavel clonar() throws FWDocumentException;
+}
